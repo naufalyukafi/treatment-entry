08f24fc33ee0b63eeade86a4a52fcfd4625f7958
naufalyukafi <naufalyukafi10@gmail.com>
2024-02-26 06:41:56 +0700

    feat: add routing api

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const { getPatient, addPatient } = require("./patient.controller");
+const { getCategory, addCategory } = require("./category.controller");
+const { getTreatment, addTreatment } = require("./treatment.controller");
+const { getMedication, addMedication } = require("./medication.controller");
 
+// Patient
 router.get("/patient", getPatient);
 router.post("/new-patient", addPatient);
+
+// Category
+router.get("/categories", getCategory);
+router.post("/new-category", addCategory);
+
+// Treatment
+router.get("/treatments", getTreatment);
+router.post("/new-treatment", addTreatment);
+
+//medications
+router.get("/medications", getMedication);
+router.post("/new-medication", addMedication);
 
 module.exports = router
